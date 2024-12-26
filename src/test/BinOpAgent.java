1cc192214cb4af5cbaac9d760e9c2ce273215529
ShirYahav <shiryahav07@gmail.com>
2024-12-26 13:57:09 +0000

    finished the third milestone

---
+++ src/test/BinOpAgent.java
@@ -0,0 +1,70 @@
+package test;
+
+import java.util.function.BinaryOperator;
+
+import test.TopicManagerSingleton.TopicManager;
+
+public class BinOpAgent implements Agent {
+
+    private final String agentName;
+    private final String inTopic1;
+    private final String inTopic2;
+    private final String outTopic;
+    private final BinaryOperator<Double> operation;
+
+    private double val1;
+    private double val2;
+
+    public BinOpAgent(String agentName, String inTopic1, String inTopic2, String outTopic, BinaryOperator<Double> operation) {
+        this.agentName = agentName;
+        this.inTopic1 = inTopic1;
+        this.inTopic2 = inTopic2;
+        this.outTopic = outTopic;
+        this.operation = operation;
+
+        TopicManager tm = TopicManagerSingleton.get();
+        tm.getTopic(inTopic1).subscribe(this);
+        tm.getTopic(inTopic2).subscribe(this);
+
+        tm.getTopic(outTopic).addPublisher(this);
+
+        reset();
+    }
+
+    @Override
+    public String getName() {
+        return agentName;
+    }
+
+    @Override
+    public void reset() {
+        val1 = 0.0;
+        val2 = 0.0;
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+
+        double newVal = msg.asDouble;
+
+        if (topic.equals(inTopic1)) {
+            val1 = newVal;
+        } else if (topic.equals(inTopic2)) {
+            val2 = newVal;
+        }
+
+        double result = operation.apply(val1, val2);
+
+        TopicManagerSingleton.get().getTopic(outTopic).publish(new Message(result));
+    }
+
+    @Override
+    public void close() {
+
+        TopicManager tm = TopicManagerSingleton.get();
+        tm.getTopic(inTopic1).unsubscribe(this);
+        tm.getTopic(inTopic2).unsubscribe(this);
+
+        tm.getTopic(outTopic).removePublisher(this);
+    }
+}
---
+++ src/test/Node.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Node {
+    private String name;
+    private List<Node> edges;
+    private Message message;
+
+    public Node(String name) {
+        this.name = name;
+        this.edges = new ArrayList<>();
+    }
+
+    public void addEdge(Node edge) {
+        edges.add(edge);
+    }
+
+    public boolean hasCycles() {
+        return hasCyclesDFS(new HashSet<>(), new HashSet<>());
+    }
+
+    private boolean hasCyclesDFS(Set<Node> visited, Set<Node> inStack) {
+        if (inStack.contains(this)) {
+            return true;
+        }
+        if (visited.contains(this)) {
+            return false;
+        }
+        visited.add(this);
+        inStack.add(this);
+        for (Node neighbor : edges) {
+            if (neighbor != null && neighbor.hasCyclesDFS(visited, inStack)) {
+                return true;
+            }
+        }
+        inStack.remove(this);
+        return false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Node> getEdges() {
+        return this.edges;
+    }
+
+    public Message getMessage() {
+        return this.message;
+    }
+
+    public void setEdges(List<Node> edges) {
+        this.edges = edges;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
